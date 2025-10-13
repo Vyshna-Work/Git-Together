ee1f3c574b30be1419a54ea36f570100345317aa
Esmee0 <esmee.moller@student.nhlstenden.com>
2025-10-13 13:46:12 +0200

    Added by Esmee

---
+++ src/Blink.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+int LED = 12;
+void setup(){
+
+    Serial.begin(9600);
+    pinMode(LED, OUTPUT);
+}
+
+void loop(){
+    digitalWrite(LED,HIGH);
+    delay(600);
+    digitalWrite(LED, LOW);
+}
