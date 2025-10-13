32beef5adeed4d965a091e711f879ceeb188406c
Vyshna-Work <vyshna.attadappa@student.nhlstenden.com>
2025-10-13 13:18:26 +0200

    Added by Vyshna

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,28 @@
 #include <Arduino.h>
 
-
-int sum(int a, int b){
-  return a+b;
-}
-void setup(){
-  Serial.begin(9600);
+void setup() 
+{
+  Serial.begin(9600);                                    // Start serial communication
 }
 
-void loop(){
-  int a, b;
+void loop() 
+{
+  calculateSum();                                        // Call your function
+  delay(3000);                                           // Wait a few seconds before asking again
+}
 
-  a = Serial.parseInt();
-  b = Serial.parseInt(); 
-  Serial.println(sum(a,b));
+void calculateSum()                                      // calculate the sum of 2 numbers
+{
+  int num1 = 0, num2 = 0;                                // variable declared
+  
+  Serial.println("Enter first number: ");                // asking user to inout first number
+  while (!Serial.available()) {}
+  num1 = Serial.parseInt();
+
+  Serial.println("Enter second number: ");              // asking user to inout first number
+  while (!Serial.available()) {}
+  num2 = Serial.parseInt();
+
+  Serial.print("The sum is: ");                         // to print the sum
+  Serial.println(num1 + num2);
 }
