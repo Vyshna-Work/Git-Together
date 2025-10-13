1b01120f0c03ea73e65e2a66f2b63a2afe3136b8
Vyshna-Work <vyshna.attadappa@student.nhlstenden.com>
2025-10-13 14:28:10 +0200

    Added by Vyshna

---
+++ src/Sum.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Enter two numbers separated by space or newline:");
+}
+
+void loop() {
+  if (Serial.available()) {
+    int num1 = Serial.parseInt();
+    int num2 = Serial.parseInt();
+    Serial.print("Sum: ");
+    Serial.println(num1 + num2);
+  }
+}
